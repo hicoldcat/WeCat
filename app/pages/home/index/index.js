2298d056fa77f2a96aa9b26a27e44c61c641857c
hicoldcat <1351290389@qq.com>
2016-10-28 18:47:50 +0800

    添加底部导航栏，使用知乎api接口完成首页home页面展示

---
+++ app/pages/home/index/index.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    topstories: [],       //照片墙
+    indicatorDots: true,   //是否显示面板指示点
+    autoplay: true,        //是否自动播放
+    interval: 3000,         //自动切换时间间隔
+    duration: 1000,         //滑动动画时长
+    storieslist:[]         //首页故事列表
+  },
+
+  onLoad:function(){
+    var that = this;
+     wx.request({
+      url: 'http://news-at.zhihu.com/api/4/news/latest',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success (res) {
+        console.log(res);
+        that.setData({
+          storieslist:res.data.stories,
+          topstories:res.data.top_stories
+        });
+      }
+    })
+  },
+  
+})
---
+++ app/pages/music/music/music.js
@@ -0,0 +1,24 @@
+Page({
+  data:{ 
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    String2
+  },
+  onReady:function(){
+    // 页面渲染完成
+    String3
+  },
+  onShow:function(){
+    // 页面显示
+   console.log(this.banner);
+  },
+  onHide:function(){
+    // 页面隐藏
+    String5
+  },
+  onUnload:function(){
+    // 页面关闭
+    String6
+  }
+})
